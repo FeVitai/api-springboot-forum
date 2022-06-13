a28925e4192286d1170922763d04f3ba541e870c
FeVitai <felipevitai93@gmail.com>
2022-06-13 16:04:30 -0300

    implementacao de permissoes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$j8aT7eT4H8t8ywfW0lz2PORP2sCbpYDnUGrXSbIUz1SGWbu8hdwla');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
